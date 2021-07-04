3a3fb739b4ddafa226a091216ed84f557e2dc2b3
Aniket <rajeshwarkumar9204@gmail.com>
2021-07-04 10:59:24 +0530

    create carousel

---
+++ public/js/carousel.js
@@ -0,0 +1,30 @@
+const carouselSlide = document.querySelectorAll(".carousel-slide");
+const imgWidth = carouselSlide[0].clientWidth;
+
+const carouselTrack = document.querySelector(".carousel-track");
+
+let activeSlide = 0;
+
+const slide = (dir) => {
+  if (dir === "prev") {
+    if (activeSlide < carouselSlide.length - 1) {
+      activeSlide++;
+    } else {
+      activeSlide = 0;
+    }
+  } else if (dir === "next") {
+    if (activeSlide > 0) {
+      activeSlide--;
+    } else {
+      activeSlide = carouselSlide.length - 1;
+    }
+  }
+  carouselTrack.setAttribute(
+    "style",
+    `transition: 250ms ease-out; transform: translateX(-${
+      activeSlide * imgWidth
+    }px)`
+  );
+};
+
+export { slide };
---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+import { slide } from "./carousel.js";
+
+const prevBtn = document.querySelector("#slide-left");
+const nextBtn = document.querySelector("#slide-right");
+
+nextBtn.addEventListener("click", () => {
+  slide("prev");
+});
+
+prevBtn.addEventListener("click", () => {
+  slide("next");
+});
