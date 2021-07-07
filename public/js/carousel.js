db137b556de2a051430e5d4b7c70baf4883c6e79
Aniket <rajeshwarkumar9204@gmail.com>
2021-07-07 14:45:43 +0530

    create working multiple-carousels

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,39 +1,30 @@
-const carousel = document.querySelectorAll(".carousel");
-carousel.forEach((el) => {
-  const carouselTrack = el.querySelector(".carousel-track");
-  const carouselSlide = el.querySelectorAll(".carousel-slide");
+// const carousel = document.querySelectorAll(".carousel");
+const slide = (carId, dir, activeSlide) => {
+  const carousel = document.querySelector(`#${carId}`);
+  const carouselTrack = carousel.querySelector(".carousel-track");
+  const carouselSlide = carousel.querySelectorAll(".carousel-slide");
   const imgWidth = carouselSlide[0].clientWidth;
-  const prevBtn = el.querySelector("#slide-left");
-  const nextBtn = el.querySelector("#slide-right");
 
-  let activeSlide = 0;
-
-  const slide = (dir) => {
-    if (dir === "prev") {
-      if (activeSlide < carouselSlide.length - 1) {
-        activeSlide++;
-      } else {
-        activeSlide = 0;
-      }
-    } else if (dir === "next") {
-      if (activeSlide > 0) {
-        activeSlide--;
-      } else {
-        activeSlide = carouselSlide.length - 1;
-      }
+  if (dir === "next") {
+    if (activeSlide < carouselSlide.length - 1) {
+      activeSlide++;
+    } else {
+      activeSlide = 0;
     }
-    carouselTrack.setAttribute(
-      "style",
-      `transition: 250ms ease-out; transform: translateX(-${
-        activeSlide * imgWidth
-      }px)`
-    );
-  };
-  nextBtn.addEventListener("click", () => {
-    slide("prev");
-  });
+  } else if (dir === "prev") {
+    if (activeSlide > 0) {
+      activeSlide--;
+    } else {
+      activeSlide = carouselSlide.length - 1;
+    }
+  }
+  carouselTrack.setAttribute(
+    "style",
+    `transition: 250ms ease-out; transform: translateX(-${
+      activeSlide * imgWidth
+    }px)`
+  );
+  return activeSlide;
+};
 
-  prevBtn.addEventListener("click", () => {
-    slide("next");
-  });
-});
+export { slide };
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,27 @@
+// Carousel
+
+import { slide } from "./carousel.js";
+
+const prevBtns = document.querySelectorAll("#slide-left");
+const nextBtns = document.querySelectorAll("#slide-right");
+
+let activeSlide = 0;
+
+prevBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const carId = btn.parentElement.parentElement.id;
+    activeSlide = slide(carId, "prev", activeSlide);
+  });
+});
+
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const carId = btn.parentElement.parentElement.id;
+    activeSlide = slide(carId, "next", activeSlide);
+    console.log("something", activeSlide);
+  });
+});
+
 // Sidebar
 
 import { toggleNav } from "./navbar.js";
@@ -6,3 +30,5 @@ const sidebar = document.querySelector(".sidebar");
 const hamburger = document.querySelector(".hamburger_menu");
 const close = document.querySelectorAll(".close-nav");
 hamburger.addEventListener("click", () => toggleNav(sidebar, close));
+
+// YouTube Images
