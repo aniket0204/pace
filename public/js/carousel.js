40961b2c0c166169e569d20fc3c610c4a2943731
Aniket <rajeshwarkumar9204@gmail.com>
2021-07-07 14:15:14 +0530

    start creating feedback section

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,30 +1,39 @@
-const carouselSlide = document.querySelectorAll(".carousel-slide");
-const imgWidth = carouselSlide[0].clientWidth;
+const carousel = document.querySelectorAll(".carousel");
+carousel.forEach((el) => {
+  const carouselTrack = el.querySelector(".carousel-track");
+  const carouselSlide = el.querySelectorAll(".carousel-slide");
+  const imgWidth = carouselSlide[0].clientWidth;
+  const prevBtn = el.querySelector("#slide-left");
+  const nextBtn = el.querySelector("#slide-right");
 
-const carouselTrack = document.querySelector(".carousel-track");
+  let activeSlide = 0;
 
-let activeSlide = 0;
-
-const slide = (dir) => {
-  if (dir === "prev") {
-    if (activeSlide < carouselSlide.length - 1) {
-      activeSlide++;
-    } else {
-      activeSlide = 0;
+  const slide = (dir) => {
+    if (dir === "prev") {
+      if (activeSlide < carouselSlide.length - 1) {
+        activeSlide++;
+      } else {
+        activeSlide = 0;
+      }
+    } else if (dir === "next") {
+      if (activeSlide > 0) {
+        activeSlide--;
+      } else {
+        activeSlide = carouselSlide.length - 1;
+      }
     }
-  } else if (dir === "next") {
-    if (activeSlide > 0) {
-      activeSlide--;
-    } else {
-      activeSlide = carouselSlide.length - 1;
-    }
-  }
-  carouselTrack.setAttribute(
-    "style",
-    `transition: 250ms ease-out; transform: translateX(-${
-      activeSlide * imgWidth
-    }px)`
-  );
-};
+    carouselTrack.setAttribute(
+      "style",
+      `transition: 250ms ease-out; transform: translateX(-${
+        activeSlide * imgWidth
+      }px)`
+    );
+  };
+  nextBtn.addEventListener("click", () => {
+    slide("prev");
+  });
 
-export { slide };
+  prevBtn.addEventListener("click", () => {
+    slide("next");
+  });
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,3 @@
-// Carousel
-
-import { slide } from "./carousel.js";
-
-const prevBtn = document.querySelector("#slide-left");
-const nextBtn = document.querySelector("#slide-right");
-
-nextBtn.addEventListener("click", () => {
-  slide("prev");
-});
-
-prevBtn.addEventListener("click", () => {
-  slide("next");
-});
-
 // Sidebar
 
 import { toggleNav } from "./navbar.js";
