b5e1f80171240963d5014a35cfae52b101adff5d
Aniket <rajeshwarkumar9204@gmail.com>
2021-07-11 20:06:31 +0530

    back to previous version

--- public/js/main.js
+++ public/js/main.js
@@ -9,9 +9,3 @@ const sidebar = document.querySelector(".sidebar");
 const hamburger = document.querySelector(".hamburger_menu");
 const close = document.querySelectorAll(".close-nav");
 hamburger.addEventListener("click", () => toggleNav(sidebar, close));
-
-// Image Gallery Popup
-
-$(".img-gallery").magnificPopup({
-  type: "image",
-});
--- src/app.js
+++ src/app.js
@@ -11,17 +11,10 @@ const port = process.env.PORT || 3000;
 const pubPath = path.join(__dirname, "../public/");
 app.use(express.static(pubPath));
 
-// jQuery
-const jquery = path.join(__dirname, "../node_modules/jquery/dist/");
-app.use("/jquery", express.static(jquery));
-
-// magnific popup
-const magnific = path.join(__dirname, "../node_modules/dist/");
-app.use("/magnific-popup", express.static(magnific));
-
-// flickity
 const flickity = path.join(__dirname, "../node_modules/flickity/dist/");
+// const flickityJS = path.join(__dirname, "..flickity/dist/");
 app.use("/flickity", express.static(flickity));
+// app.use("/flickitycss", express.static(flickityJS));
 
 // Setting up view engine
 app.set("view engine", "pug");
