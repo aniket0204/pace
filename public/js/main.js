5b24d2968196d917785b56ca59bd810e7f5813d4
Aniket <rajeshwarkumar9204@gmail.com>
2021-07-12 18:32:04 +0530

    add zoom effect on image-gallery

--- public/js/main.js
+++ public/js/main.js
@@ -18,5 +18,17 @@ $(document).ready(function () {
     gallery: {
       enabled: true,
     },
+    mainClass: "mfp-with-zoom",
+    zoom: {
+      enabled: true,
+
+      duration: 300,
+      easing: "ease-in-out",
+      opener: function (openerElement) {
+        return openerElement.is("img")
+          ? openerElement
+          : openerElement.find("img");
+      },
+    },
   });
 });
